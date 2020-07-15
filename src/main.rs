8ec247aa2e6fd43d0bd0ffba63432d416c230790
Shintaro <shintaro.sakata.tokyo@gmail.com>
2020-07-15 13:29:40 +0900

    add req example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+mod schema;
+
+use reqwest::blocking as req;
+use std::collections::HashMap;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp: HashMap<String, String> = req::get("https://httpbin.org/ip")?.json()?;
+    println!("{:#?}", resp);
+    Ok(())
 }
